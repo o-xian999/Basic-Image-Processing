4fdb75368cf48be6a8da8c91ed70a719387da5d7
o-xian999 <onoyama1999910@gmail.com>
2020-10-27 15:25:22 +0900

    Edit task01.c

--- task01.c
+++ task01.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <opencv2/oppencv.hpp>
+#include <opencv2/opencv.hpp>
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp> 
 
@@ -17,23 +17,31 @@ int main(int argc, char* argv[]) {
   img_src = imread(argv[1], 0);
   img_dst = Mat::zeros(img_src.rows, img_src.cols, CV_8UC1);
   for (int y = 0; y < img_src.rows; y++) {
-    for (int x  = 0; x < img_src.cols; x++) {
+    for (int x = 0; x < img_src.cols; x++) {
       v_src = getV(img_src, x, y);
-      if (v_src < 64) {
+      if (v_src < 32) {
         v_dst = 0;
+      } else if (v_src < 64) {
+        v_dst = 36;
+      } else if (v_src < 96) {
+        v_dst = 72;
       } else if (v_src < 128) {
-        v_dst = 85;
-      } else if (v_srd < 192) {
-        v_dst = 170;
-      } else {
-        v_dst  =255;
+        v_dst = 108;
+      } else if (v_src < 160) {
+        v_dst = 144;
+      } else if (v_src < 192) {
+        v_dst = 180;
+      } else if (v_src < 224) {
+        v_dst = 216
+      } else if {
+        v_dst = 255;
       }
       setV(img_dst, x, y, v_dst);
     }
   }
 
-  nameWindow(argv[1], CV_WINDOW_AUTOSIZE);
-  nameWindow("output", CV_WINDOW_AUTOSIZE);
+  namedWindow(argv[1], CV_WINDOW_AUTOSIZE);
+  namedWindow("output", CV_WINDOW_AUTOSIZE);
 
   while (1) {
     imshow(argv[1], img_src);
