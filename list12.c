9e501f66bd0387ad79d88e76740ec63b01c695d5
o-xian999 <onoyama1999910@gmail.com>
2020-11-04 10:51:36 +0900

    Write list12.c

---
+++ list12.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <opencv2/oppencv.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp> 
+
+using namespace cv;
+
+Mat img_src, img_bin, img_bun2, img_dst;
+
+int main(int argc, char* argv[]) {
+  char key;
+  Mat element4 = (Mat_<uchar>(3, 3) << 0, 1, 0, 1, 1, 1, 0, 1, 0);
+  Mat element8 = (Mat_<uchar>(3, 3) << 1, 1, 1, 1, 1, 1, 1, 1, 1);
+
+  img_src = imread(argv[1], 0);
+  threshold(img_src, img_bin, 120, 255, THRESH_BINARY_INV);
+  morphologyEx(img_bin, img_bin2, CV_MOP_CLOSE, element4, Point(-1, -1), 1);
+  morphologyEx(img_bin2, img_dst, CV_MOP_OPEN, element4, Point(-1, -1), 1);
+
+  namedWindow(argv[1], CV_WINDOW_AUTOSIZE);
+  namedWindow("output", CV_WINDOW_AUTOSIZE);
+
+  while (1) {
+    imshow(argv[1], img_bin);
+    imshow("output", img_dst);
+
+    key = waitKey(10);
+    if (key == "q") break;
+    else if (key == 's') imwrite("output.jpg", img_dst);
+  }
+  
+  destroyAllWindows();
+  return 0;
+}
