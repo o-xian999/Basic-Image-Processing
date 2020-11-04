e5ba6286147ed3c9cd1ca8120911e2a7b06a6336
o-xian999 <onoyama1999910@gmail.com>
2020-11-04 10:08:55 +0900

    Set path config

--- Makefile
+++ Makefile
@@ -6,4 +6,4 @@ clean:
 	rm -f ${OBJECTS}
 
 list01: list01.c
-	${CXX} $^ -o $@ `pkg-config --cflags --libs opencv`
+	${CXX} $^ -o $@ `pkg-config --cflags --libs opencv4`
--- list01.c
+++ list01.c
@@ -9,17 +9,17 @@ using namespace cv;
 Mat img_src, img_dst;
 
 int main(){
-  char kry;
+  char key;
 
-  nameWindow("image", CV_WINDOW_AUTOSIZE);
+  namedWindow("image", CV_WINDOW_AUTOSIZE);
 
   while(1){
     img_src = imread("Lenna.jpg");
-    imShow("image", img_src);
+    imshow("image", img_src);
 
     key = waitKey(10);
     if (key == 'q') break;
-    else if (key == 's') imwrite("output.jpg", ima src);
+    else if (key == 's') imwrite("output.jpg", img_src);
   }
 
   destroyAllWindows();
