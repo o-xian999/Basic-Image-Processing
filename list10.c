5e7a4c853534c01b6b3170da5d51fda124a9ce67
o-xian999 <onoyama1999910@gmail.com>
2020-11-04 10:42:23 +0900

    Write list10.c

---
+++ list10.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <opencv2/oppencv.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp> 
+
+using namespace cv;
+
+Mat img_src, img_dst;
+
+int main(int argc, char* argv[]) {
+  char key;
+
+  img_src = imread(argv[1], 0);
+  threshold(img_src, img_dst, 120, 255, THRESH_BINARY);
+
+  namedWindow(argv[1], CV_WINDOW_AUTOSIZE);
+  namedWindow("output", CV_WINDOW_AUTOSIZE);
+
+  while (1) {
+    imshow(argv[1], img_src);
+    imshow("output", img_dst);
+
+    key = waitKey(10);
+    if (key == "q") break;
+    else if (key == 's') imwrite("output.jpg", img_dst);
+  }
+  
+  destroyAllWindows();
+  return 0;
+}
