001ddaab758c66da40b595cafde623a15461d1b2
o-xian999 <onoyama1999910@gmail.com>
2020-11-07 13:09:10 +0900

    Checking file behavior

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-OBJECTS = list01 list02
+OBJECTS = list01 list02 list03
 
 all: ${OBJECTS}
 
@@ -9,4 +9,7 @@ list01: list01.c
 	${CXX} $^ -o $@ `pkg-config --cflags --libs opencv4`
 
 list02: list02.c
+	${CXX} $^ -o $@ `pkg-config --cflags --libs opencv4`
+
+list03: list03.c
 	${CXX} $^ -o $@ `pkg-config --cflags --libs opencv4`
--- list03.c
+++ list03.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
-#include <opencv2/oppencv.hpp>
+#include <opencv2/opencv.hpp>
 #include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui_c.h>
 #include <opencv2/highgui/highgui.hpp> 
 
-#define getB(img, x, y) (img.ptr<ucahr>(y)[(x)*3])
-#define getG(img, x, y) (img.ptr<ucahr>(y)[(x)*3+1])
-#define getR(img, x, y) (img.ptr<ucahr>(y)[(x)*3+2])
+#define getB(img, x, y) (img.ptr<uchar>(y)[(x)*3])
+#define getG(img, x, y) (img.ptr<uchar>(y)[(x)*3+1])
+#define getR(img, x, y) (img.ptr<uchar>(y)[(x)*3+2])
 
 using namespace cv;
 
@@ -15,22 +16,23 @@ void on_mouse(int event, int x, int y, int flag, void *param){
   uchar r, g, b;
 
   switch(event){
-    case CV_EVENT_LBUTTONDOWN:
-      r = getR(img, x, y);
-      g = getG(img, x, y);
-      b = getB(img, x, y);
+    case EVENT_LBUTTONDOWN:
+      r = getR(img_src, x, y);
+      g = getG(img_src, x, y);
+      b = getB(img_src, x, y);
       printf("(x:%3d y:%3d) (R:%3d G:%3d B:%3d)\n", x, y, r, g, b);
       break;
   }
 }
 
 int main () {
-  cahr key;
+  char key;
 
   VideoCapture cap(0);
-  cap.set(CV_CAP_PROP_FRAME_WIDTH, 640);
-  cap.set(CV_CAP_PROP_FRAME_HEIGHT, 480);
-  namedWindow("camera", on_mouse, NULL);
+  cap.set(CAP_PROP_FRAME_WIDTH, 640);
+  cap.set(CAP_PROP_FRAME_HEIGHT, 480);
+  namedWindow("camera", WINDOW_AUTOSIZE);
+  cvSetMouseCallback("camera", on_mouse, NULL);
 
   while (1) {
     cap >> img_src;
@@ -41,6 +43,6 @@ int main () {
     else if (key == 's') imwrite("output.jpg", img_src);
   }
 
-  destroyAllWindow();
+  destroyAllWindows();
   return 0;
 }
